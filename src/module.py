394622ae9ea0c9cef750dd6af8a0a854e06c6bb0
julieta luzzi <luzzijulieta@gmail.com>
2018-10-19 15:47:24 -0300

    fixes

--- src/module.py
+++ src/module.py
@@ -10,10 +10,11 @@ def start():
     print "module is running"
 
     # reading the ratings data
-    ratings = pd.read_csv('ml-1m/ratings.dat', sep="::", header=None, engine='python')
+    ratings = pd.read_csv('dataset/ratings.csv', header=1, engine='python')
 
     # Lets pivot the data to get it at a user level
-    ratings_pivot = pd.pivot_table(ratings[[0, 1, 2]], values=2, index=0, columns=1).fillna(0)
+    ratings_pivot = pd.pivot_table(ratings[[0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]], values=2, index=0, columns=1)\
+        .fillna(0)
 
     # creating train and test sets
     x_train, x_test = train_test_split(ratings_pivot, train_size=0.8)
@@ -32,7 +33,7 @@ def start():
     input_layer = tf.placeholder('float', [None, 3706])
     # add a constant node to the first layer
     # it needs to have the same shape as the input layer for me to be
-    # able to concatinate it later
+    # able to concatenate it later
     input_layer_const = tf.fill([tf.shape(input_layer)[0], 1], 1.0)
     input_layer_concat = tf.concat([input_layer, input_layer_const], 1)
     # multiply output of input_layer wth a weight matrix
@@ -54,13 +55,24 @@ def start():
     init = tf.global_variables_initializer()
     sess = tf.Session()
     sess.run(init)
+
     # defining batch size, number of epochs and learning rate
     batch_size = 100  # how many images to use together for training
     hm_epochs = 200  # how many times to go through the entire dataset
-    tot_users = x_train.shape[0]  # total number of images
+    tot_images = x_train.shape[0]  # total number of images
 
     # running the model for a 200 epochs taking 100 users in batches
     # total improvement is printed out after each epoch
+    training(batch_size, hm_epochs, tot_images, input_layer, meansq, optimizer, output_layer, output_true, sess, x_test, x_train)
+
+    # pick a user
+    sample_user = x_test.iloc[99, :]
+    # get the predicted ratings
+    sample_user_pred = sess.run(output_layer, feed_dict={input_layer: [sample_user]})
+    print sample_user_pred
+
+
+def training(batch_size, hm_epochs, tot_images, input_layer, meansq, optimizer, output_layer, output_true, sess, x_test, x_train):
     for epoch in range(hm_epochs):
         epoch_loss = 0  # initializing error as 0
 
@@ -74,11 +86,6 @@ def start():
 
         print('MSE train', mse(output_train, x_train), 'MSE test', mse(output_test, x_test))
         print('Epoch', epoch, '/', hm_epochs, 'loss:', epoch_loss)
-
-    # pick a user
-    sample_user = x_test.iloc[99, :]
-    # get the predicted ratings
-    sample_user_pred = sess.run(output_layer, feed_dict={input_layer: [sample_user]})
 
 
 if __name__ == "__main__":
